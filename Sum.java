3b6bff244a401f62975181419875f89cb07383ff
shashidhar <shashidhar.b@itprofound.com>
2024-01-04 11:25:27 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -3,8 +3,7 @@ public class Sum{
     public static void main(String[] args){
         int a=10;
         int b=20;
-
         int c=a+b;
-        System.out.println(c);
+        System.out.println("This sum is" + (a+b) );
     }
 }
